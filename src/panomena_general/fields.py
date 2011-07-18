4cc044ee859f4fca7ee2ae631bf94c6159b16400
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-07-18 13:10:22 +0200

    Added comma separated lookup field

--- src/panomena_general/fields.py
+++ src/panomena_general/fields.py
@@ -1,11 +1,12 @@
 from django import forms
+from django.db.models import Q
 from django.forms import widgets
 from django.core.validators import email_re
 from django.utils.translation import ugettext_lazy as _
 
 
-class CommaSeparatedEmailInput(widgets.Textarea):
-    """Comma separated email input widget."""
+class CommaSeparatedInput(widgets.Textarea):
+    """Comma separated input widget."""
     input_type = 'text'
     
     def render(self, name, value, attrs=None):
@@ -13,13 +14,13 @@ class CommaSeparatedEmailInput(widgets.Textarea):
             value = ''
         elif isinstance(value, (list, tuple)):
             value = (', '.join([user.username for user in value]))
-        return super(CommaSeparatedEmailInput, self).render(name, value, attrs)
+        return super(CommaSeparatedInput, self).render(name, value, attrs)
 
 
 class CommaSeparatedEmailField(forms.Field):
-    """Comman seperated email form field."""
+    """Comma seperated email form field."""
 
-    widget = CommaSeparatedEmailInput
+    widget = CommaSeparatedInput
     
     def clean(self, value):
         value = super(CommaSeparatedEmailField, self).clean(value)
@@ -41,4 +42,31 @@ class CommaSeparatedEmailField(forms.Field):
                 _("The following addresses are incorrect: %s" \
                 % ', '.join(invalid)))
         return emails
+
+
+class CommaSeparatedLookupField(forms.Field):
+    """Field for looking up an array of objects using values of
+    specified model fields.
+    
+    """
+
+    widget = CommaSeparatedInput
+
+    def __init__(self, model, fields, *args, **kwargs):
+        self.model = model
+        self.fields = fields
+        super(CommaSeparatedLookupField, self).__init__(*args, **kwargs)
+
+    def clean(self, value):
+        value = super(CommaSeparatedLookupField, self).clean(value)
+        # return empty list for empty field
+        if not value: return []
+        # build the query
+        query = Q()
+        values = [v.strip() for v in value.split(',')]
+        for field in self.fields:
+            key = '%s__in' % field
+            query = query | Q(**{key: values})
+        # return the results
+        return self.model.objects.filter(query).all()
 
